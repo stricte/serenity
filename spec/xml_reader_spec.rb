d56c111852232d81ee5b0ff661de8da3d06f4ab0
Jan Topiński <jan.topinski@in4mates.com>
2013-08-27 12:16:34 +0200

    Remove style from code tags

--- spec/xml_reader_spec.rb
+++ spec/xml_reader_spec.rb
@@ -42,6 +42,57 @@ module Serenity
         idx += 1
       end
     end
+    it "should ignore tags in controls and templates" do
+      xml  = <<-EOF
+          <office:body><text:p style="Standard">This is<text:span text:style-name="T10">
+          </text:span>a sentence</text:p><text:s>{%= <text:span text:style-name="T10">yeah</text:span> %}</text:s></office:body>
+      EOF
+      expected = [Node.new('          ', NodeType::TEMPLATE),
+                  Node.new('<office:body>', NodeType::TAG),
+                  Node.new('<text:p style="Standard">', NodeType::TAG),
+                  Node.new('This is', NodeType::TEMPLATE),
+                  Node.new('<text:span text:style-name="T10">', NodeType::TAG),
+                  Node.new("\n          ", NodeType::TEMPLATE),
+                  Node.new('</text:span>', NodeType::TAG),
+                  Node.new('a sentence', NodeType::TEMPLATE),
+                  Node.new('</text:p>', NodeType::TAG),
+                  Node.new('<text:s>', NodeType::TAG),
+                  Node.new('{%= yeah %}', NodeType::TEMPLATE),
+                  Node.new('</text:s>', NodeType::TAG),
+                  Node.new('</office:body>', NodeType::TAG),
+                  Node.new("\n          {% for row in rows %}\n          ", NodeType::CONTROL),
+                  Node.new('</office:document-content>', NodeType::TAG)]
+      reader = XmlReader.new xml
+
+      idx = 0
+      reader.each_node do |node, type|
+        node.should == expected[idx].text
+        type.should == expected[idx].type
+        idx += 1
+      end
+
+    end
+    it "should unescape xml" do
+      xml  = <<-EOF
+              <text:p text:style-name="P1"><text:span text:style-name="T1">{%=</text:span>
+              <text:span text:style-name="T2">@data.applicants.applicant[1][&apos;first-name&apos;]</text:span><text:span text:style-name="T1">%}</text:span></text:p>
+      EOF
+      expected = [Node.new('              ', NodeType::TEMPLATE),
+                  Node.new('<text:p text:style-name="P1">', NodeType::TAG),
+                  Node.new('<text:span text:style-name="T1">', NodeType::TAG),
+                  Node.new("{%=\n              @data.applicants.applicant[1]['first-name']%}", NodeType::TEMPLATE),
+                  Node.new('</text:span>', NodeType::TAG),
+                  Node.new('</text:p>', NodeType::TAG)]
+      reader = XmlReader.new xml
+
+      idx = 0
+      reader.each_node do |node, type|
+        node.should == expected[idx].text
+        type.should == expected[idx].type
+        idx += 1
+      end
+
+    end
   end
 end
 
