f055d34c718e554d3c064af68b1785b91950053e
Mariusz Michalak <stricte@gumed.edu.pl>
2016-05-17 21:58:37 +0200

    There is no need to build own path if we already have it in href attribute

--- lib/serenity/images_processor.rb
+++ lib/serenity/images_processor.rb
@@ -2,8 +2,6 @@ module Serenity
   class ImagesProcessor
     include Debug
 
-    IMAGE_DIR_NAME = "Pictures"
-
     def initialize(xml_content, context)
       @replacements = []
       @images = eval('@images', context)
@@ -19,9 +17,8 @@ module Serenity
         @images.each do |image_name, replacement_path|
           if node = xml_data.xpath("//draw:frame[@draw:name='#{image_name}']/draw:image").first
             placeholder_path = node.attribute('href').value
-            odt_image_path = ::File.join(IMAGE_DIR_NAME, ::File.basename(placeholder_path))
 
-            @replacements << [odt_image_path, replacement_path]
+            @replacements << [placeholder_path, replacement_path]
           end
         end
       end
